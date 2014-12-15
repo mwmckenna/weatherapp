bbd4eb8bedc13c29405a307e05cc120d8ad2fd13
MWMCKENNA <MWMCKENNA@GMAIL.COM>
2014-12-15 13:09:28 -0800

    working on models

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -1,2 +1,21 @@
 class Outfit < ActiveRecord::Base
+  belongs_to :user
+
+  has_many :favorites
+  has_many :favorited, through: :favorites, source: :user
+  # has_many :taco, through: :join_table, source: :model_trying_to_return
+
+  def self.temp_constraints temp, user=nil
+    if user
+      return fun_outfits(temp).where("id not in (?)", user.favorited.select("outfits.id"))
+    end
+
+    fun_outfits temp
+  end
+
+  private
+
+  def self.fun_outfits temp
+     where("temp_min <= ").where("temp_max IS NULL OR time_max >= ")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :username,
+    presence: true,
+    uniqueness: true
+
+  has_many :outfits
+
+  has_many :favorites
+  has_many :favorited, through: :favorites, source: :outfit
+  # has_many :taco, through: :join_table, source: :model_trying_to_return
 end
